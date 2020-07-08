110ee86da2f68710e7f460389ab6e8ff6b653c61
GeorgiTsekov <53428844+GeorgiTsekov@users.noreply.github.com>
2020-07-08 19:52:10 +0300

    Add

---
+++ Exam/P01ClubParty/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01ClubParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int capacity = int.Parse(Console.ReadLine());
+
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            List<int> integers = new List<int>();
+            Queue<string> halls = new Queue<string>();
+            int safeCapacity = capacity;
+
+            Stack<string> elements = new Stack<string>(input);
+
+            while (elements.Count > 0)
+            {
+                string currentElement = elements.Pop();
+
+                bool isNumber = int.TryParse(currentElement, out int parsedNumber);
+
+                if (!isNumber)
+                {
+                    halls.Enqueue(currentElement);
+                }
+                else
+                {
+                    if (safeCapacity - parsedNumber < 0 && halls.Count > 0)
+                    {
+                        Console.WriteLine($"{halls.Dequeue()} -> " + string.Join(", ", integers));
+                        integers.Clear();
+                        safeCapacity = capacity;
+                        if (halls.Count > 0)
+                        {
+                            safeCapacity -= parsedNumber;
+                            integers.Add(parsedNumber);
+                        }
+                    }
+                    else if (safeCapacity - parsedNumber >= 0 && halls.Count > 0)
+                    {
+                        integers.Add(parsedNumber);
+                        safeCapacity -= parsedNumber;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Exam/P02TronRacers/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+
+namespace P02TronRacers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[][] squareMatrix = new char[n][];
+
+            int firstPlayerRow = 0;
+            int firstPlayerCol = 0;
+            int secondPlayerRow = 0;
+            int secondPlayerCol = 0;
+
+            for (int i = 0; i < squareMatrix.Length; i++)
+            {
+                char[] array = Console.ReadLine().ToCharArray();
+                squareMatrix[i] = new char[array.Length];
+                for (int j = 0; j < array.Length; j++)
+                {
+                    squareMatrix[i][j] = array[j];
+                    if (array[j] == 'f')
+                    {
+                        firstPlayerRow = i;
+                        firstPlayerCol = j;
+                    }
+                    else if (array[j] == 's')
+                    {
+                        secondPlayerRow = i;
+                        secondPlayerCol = j;
+                    }
+                }
+            }
+
+            while (true)
+            {
+                string[] command = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                switch (command[0])
+                {
+                    case "up":
+                        firstPlayerRow--;
+                        if (firstPlayerRow < 0)
+                        {
+                            firstPlayerRow = n - 1;
+                        }
+                        break;
+                    case "down":
+                        firstPlayerRow++;
+                        if (firstPlayerRow == squareMatrix.Length)
+                        {
+                            firstPlayerRow = 0;
+                        }
+                        break;
+                    case "left":
+                        firstPlayerCol--;
+                        if (firstPlayerCol < 0)
+                        {
+                            firstPlayerCol = n - 1;
+                        }
+                        break;
+                    case "right":
+                        firstPlayerCol++;
+                        if (firstPlayerCol == squareMatrix[firstPlayerRow].Length)
+                        {
+                            firstPlayerCol = 0;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+                if (squareMatrix[firstPlayerRow][firstPlayerCol] == 's')
+                {
+                    squareMatrix[firstPlayerRow][firstPlayerCol] = 'x';
+                    break;
+                }
+                else
+                {
+                    squareMatrix[firstPlayerRow][firstPlayerCol] = 'f';
+                }
+
+                switch (command[1])
+                {
+                    case "up":
+                        secondPlayerRow--;
+                        if (secondPlayerRow < 0)
+                        {
+                            secondPlayerRow = n - 1;
+                        }
+                        break;
+                    case "down":
+                        secondPlayerRow++;
+                        if (secondPlayerRow >= squareMatrix.Length)
+                        {
+                            secondPlayerRow = 0;
+                        }
+                        break;
+                    case "left":
+                        secondPlayerCol--;
+                        if (secondPlayerCol < 0)
+                        {
+                            secondPlayerCol = n - 1;
+                        }
+                        break;
+                    case "right":
+                        secondPlayerCol++;
+                        if (secondPlayerCol >= squareMatrix[secondPlayerRow].Length)
+                        {
+                            secondPlayerCol = 0;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+                if (squareMatrix[secondPlayerRow][secondPlayerCol] == 'f')
+                {
+                    squareMatrix[secondPlayerRow][secondPlayerCol] = 'x';
+                    break;
+                }
+                else
+                {
+                    squareMatrix[secondPlayerRow][secondPlayerCol] = 's';
+                }
+            }
+
+            for (int i = 0; i < squareMatrix.Length; i++)
+            {
+                for (int j = 0; j < squareMatrix[i].Length; j++)
+                {
+                    Console.Write(squareMatrix[i][j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
