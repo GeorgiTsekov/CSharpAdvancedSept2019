2f13732f4d3f81e932035044e855e904f0958ac3
GeorgiTsekov <53428844+GeorgiTsekov@users.noreply.github.com>
2020-07-08 19:16:00 +0300

    add

---
+++ DefiningClasses/Familly.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Familly
+    {
+        private List<Person> people;
+
+        public Familly()
+        {
+            this.people = new List<Person>();
+        }
+
+        public Person Person { get; set; }
+        
+        public void AddMember(Person member)
+        {
+            this.people.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            var olderMember = people
+                .OrderByDescending(x => x.Age)
+                .FirstOrDefault();
+
+            return olderMember;
+        }
+
+        public List<Person> GetAll30PlusMembers()
+        {
+            List<Person> all30PlusMembers = people.Where(x => x.Age > 30).OrderBy(x => x.Name).ToList();
+
+            return all30PlusMembers;
+        }
+    }
+}
---
+++ DefiningClasses/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int membersCount = int.Parse(Console.ReadLine());
+
+            Familly people = new Familly();
+
+            for (int i = 0; i < membersCount; i++)
+            {
+                string[] personNameAndAge = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string personName = personNameAndAge[0];
+                int personAge = int.Parse(personNameAndAge[1]);
+
+                var person = new Person(personName, personAge);
+
+                people.AddMember(person);
+            }
+
+            var all30PlusMembers = people.GetAll30PlusMembers();
+
+            foreach (var person in all30PlusMembers)
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+    }
+}
