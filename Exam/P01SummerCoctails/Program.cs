91f1ef40dfb52b2b9b8166600744fdc36c0f3cef
GeorgiTsekov <53428844+GeorgiTsekov@users.noreply.github.com>
2020-07-08 19:57:05 +0300

    add

---
+++ Exam/P01SummerCoctails/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01SummerCoctails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ingredients = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] freshness = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .Reverse()
+                .ToArray();
+
+            Dictionary<string, int> coctails = new Dictionary<string, int>();
+
+            coctails.Add("Mimosa", 0);
+            coctails.Add("Daiquiri", 0);
+            coctails.Add("Sunshine", 0);
+            coctails.Add("Mojito", 0);
+
+            int ingredientsLeft = 0;
+
+            int zeroCounter = 0;
+
+            for (int i = 0; i < ingredients.Length; i++)
+            {
+                if (ingredients[i] != 0)
+                {
+                    if (i - zeroCounter < freshness.Length)
+                    {
+                        int multiply = ingredients[i] * freshness[i - zeroCounter];
+
+                        if (multiply == 150)
+                        {
+                            coctails["Mimosa"]++;
+                        }
+                        else if (multiply == 250)
+                        {
+                            coctails["Daiquiri"]++;
+                        }
+                        else if (multiply == 300)
+                        {
+                            coctails["Sunshine"]++;
+                        }
+                        else if (multiply == 400)
+                        {
+                            coctails["Mojito"]++;
+                        }
+                        else
+                        {
+                            ingredientsLeft += ingredients[i] + 5;
+                        }
+                    }
+                    else
+                    {
+                        ingredientsLeft += ingredients[i];
+                    }
+                }
+                else
+                {
+                    zeroCounter++;
+                }
+            }
+
+            var realCoctailsOnly = coctails.Where(x => x.Value > 0).OrderBy(x => x.Key);
+
+            if (realCoctailsOnly.Count() == 4)
+            {
+                Console.WriteLine("It's party time! The cocktails are ready!");
+            }
+            else
+            {
+                Console.WriteLine("What a pity! You didn't manage to prepare all cocktails.");
+            }
+
+            if (ingredientsLeft > 0)
+            {
+                Console.WriteLine($"Ingredients left: {ingredientsLeft}");
+            }
+
+            foreach (var coctail in realCoctailsOnly)
+            {
+                Console.WriteLine($" # {coctail.Key} --> {coctail.Value}");
+            }
+        }
+    }
+}
---
+++ Exam/P02SeashellTreasure/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P02SeashellTreasure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[][] jaggedArray = new char[n][];
+
+            for (int row = 0; row < jaggedArray.Length; row++)
+            {
+                string[] inputNumbers = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                jaggedArray[row] = new char[inputNumbers.Length];
+
+                for (int col = 0; col < jaggedArray[row].Length; col++)
+                {
+                    jaggedArray[row][col] = char.Parse(inputNumbers[col]);
+                }
+            }
+
+            string input;
+
+            List<char> collectedSeashells = new List<char>();
+
+            int stolenSeashells = 0;
+
+            while ((input = Console.ReadLine()) != "Sunset")
+            {
+                string[] command = input
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                switch (command[0])
+                {
+                    case "Collect":
+                        int row = int.Parse(command[1]);
+
+                        int col = int.Parse(command[2]);
+
+                        CollectSeashells(jaggedArray, collectedSeashells, row, col);
+
+                        break;
+                    case "Steal":
+                        row = int.Parse(command[1]);
+
+                        col = int.Parse(command[2]);
+
+                        string direction = command[3];
+
+                        if (direction == "up")
+                        {
+                            for (int i = 0; i < 4; i++)
+                            {
+                                if (row >= 0)
+                                {
+                                    stolenSeashells = StolSeashells(jaggedArray, stolenSeashells, row, col);
+                                    row--;
+                                }
+                            }
+                        }
+                        else if (direction == "down")
+                        {
+                            for (int i = 0; i < 4; i++)
+                            {
+                                if (row < jaggedArray.Length)
+                                {
+                                    stolenSeashells = StolSeashells(jaggedArray, stolenSeashells, row, col);
+                                    row++;
+                                }
+                            }
+                            
+                        }
+                        else if (direction == "left")
+                        {
+                            for (int i = 0; i < 4; i++)
+                            {
+                                if (col >= 0)
+                                {
+                                    stolenSeashells = StolSeashells(jaggedArray, stolenSeashells, row, col);
+                                    col--;
+                                }
+                            }
+                        }
+                        else if (direction == "right")
+                        {
+                            for (int i = 0; i < 4; i++)
+                            {
+                                if (col < jaggedArray[row].Length)
+                                {
+                                    stolenSeashells = StolSeashells(jaggedArray, stolenSeashells, row, col);
+                                    col++;
+                                }
+                            }
+                        }
+
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            foreach (var array in jaggedArray)
+            {
+                Console.WriteLine(string.Join(" ", array));
+            }
+
+            if (collectedSeashells.Count > 0)
+            {
+                Console.WriteLine($"Collected seashells: {collectedSeashells.Count} -> {string.Join(", ", collectedSeashells)}");
+            }
+            else
+            {
+                Console.WriteLine($"Collected seashells: {collectedSeashells.Count}");
+            }
+
+            Console.WriteLine($"Stolen seashells: {stolenSeashells}");
+        }
+
+        private static int StolSeashells(char[][] jaggedArray, int stolenSeashells, int row, int col)
+        {
+            if (IndexIsReal(jaggedArray, row, col))
+            {
+                if (jaggedArray[row][col] != '-')
+                {
+                    stolenSeashells++;
+                    jaggedArray[row][col] = '-';
+                }
+            }
+
+            return stolenSeashells;
+        }
+
+        private static void CollectSeashells(char[][] jaggedArray, List<char> collectedSeashells, int row, int col)
+        {
+            if (IndexIsReal(jaggedArray, row, col))
+            {
+                if (jaggedArray[row][col] != '-')
+                {
+                    collectedSeashells.Add(jaggedArray[row][col]);
+                    jaggedArray[row][col] = '-';
+                }
+            }
+        }
+
+        private static bool IndexIsReal(char[][] jaggedArray, int row, int col)
+        {
+            bool result = jaggedArray.Length > row &&
+                            jaggedArray[row].Length > col &&
+                            row >= 0 &&
+                            col >= 0;
+
+            return result;
+        }
+    }
+}
